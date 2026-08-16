c8b9938fb81fdb6ca4feb1d6154448b54fb66c5b
grtykert <borovyk.artyom@gmail.com>
2026-08-16 09:30:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ def run_web():
 
 threading.Thread(target=run_web, daemon=True).start()
 
-TOKEN = "8957555829:AAHtDCNwFA2OIP1VQHXPunYXscEtRlxM37k"
+TOKEN = "8957555829:AAFXEQ7b24M5YMbnZpRB8cYLnSi-VL6zraY"
 bot = telebot.TeleBot(TOKEN)
 
 
