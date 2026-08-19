bbe07e268537a09ab997bd2c5330e7ee55ef4721
grtykert <borovyk.artyom@gmail.com>
2026-08-19 14:03:33 +0300

    Fix API_TOKEN formatting in bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ def run_dummy_server():
 threading.Thread(target=run_dummy_server, daemon=True).start()
 
 # --- БОТ И БЭКАП ГРУППА ---
-API_TOKEN = "8957555829:AAFXEQ7b24M5YMbnZpRB8cYLnSi-VL6zraY
+API_TOKEN = "8957555829:AAFXEQ7b24M5YMbnZpRB8cYLnSi-VL6zraY"
 BACKUP_CHANNEL_ID = -1004445455425
 
 bot = telebot.TeleBot(API_TOKEN)
