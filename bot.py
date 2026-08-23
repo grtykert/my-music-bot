2a259f4df31d5600ac95b15a9ab0eccadce167df
grtykert <borovyk.artyom@gmail.com>
2026-08-23 12:44:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -529,7 +529,7 @@ def handle_download_callback(call):
         if audio_filename and os.path.exists(audio_filename):
             try: os.remove(audio_filename)
             except: pass
-                if thumbnail_filename and os.path.exists(thumbnail_filename):
+        if thumbnail_filename and os.path.exists(thumbnail_filename):
             try: os.remove(thumbnail_filename)
             except: pass
 
